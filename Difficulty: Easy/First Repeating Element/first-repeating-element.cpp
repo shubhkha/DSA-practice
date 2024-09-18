f62c2efddea80807dcbdf7cd8e13bf0aef7b1d41
shubhkha <157620793+shubhkha@users.noreply.github.com>
2024-09-18 23:44:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(vector<int> &arr) {
+        // code here
+        map<int,int>mpp;
+        for(int i = 0; i<arr.size(); i++){
+            mpp[arr[i]]++;
+        }
+        for(int i = 0; i<arr.size(); i++){
+            if(mpp[arr[i]] >= 2){
+                return i+1;
+                break;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.firstRepeated(arr) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
