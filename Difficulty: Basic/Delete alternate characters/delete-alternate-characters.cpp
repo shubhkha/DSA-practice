e27db0e1fbfc5ed499b952c83935bce01b72ed40
shubhkha <157620793+shubhkha@users.noreply.github.com>
2024-10-09 17:37:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Delete alternate characters/delete-alternate-characters.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string delAlternate(string S) {
+        // code here
+        string temp = "";
+        for(int i = 0; i<S.size(); i++){
+            if(i % 2 == 0){
+                temp.push_back(S[i]);
+            }
+        }
+        return temp;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.delAlternate(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
