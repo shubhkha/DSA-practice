b6e1eafb5ff460abf868350ea00c2f68d84b4798
shubhkha <157620793+shubhkha@users.noreply.github.com>
2024-10-16 23:11:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse array in groups/reverse-array-in-groups.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void reverseInGroups(vector<long long int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        for(int i = 0; i < n; i+=k){
+            int start = i;
+            int end = min(i+k-1, n-1);
+            while(start <= end){
+                swap(arr[start], arr[end]);
+                start++;
+                end--;
+            }
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k;
+        cin >> k;
+        vector<long long int> arr;
+        string input;
+        cin.ignore();
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        ob.reverseInGroups(arr, k);
+        for (long long i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
