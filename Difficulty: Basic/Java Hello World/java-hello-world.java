aba311acdb71ad3fdf4060d2353d419c02be933b
shubhkha <157620793+shubhkha@users.noreply.github.com>
2024-12-25 15:47:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Hello World/java-hello-world.java
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Solution ob = new Solution();
+        ob.printHelloWorld();
+        System.out.println("~");
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static void printHelloWorld() {
+        // code here
+        System.out.println("Hello World");
+    }
+}
