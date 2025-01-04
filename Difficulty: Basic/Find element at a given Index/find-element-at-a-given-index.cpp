e71c52584b9f8f6d6b8714acd9a36a474d48d80e
shubhkha <157620793+shubhkha@users.noreply.github.com>
2025-01-04 13:04:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find element at a given Index/find-element-at-a-given-index.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findElementAtIndex(int key, vector<int> &arr) {
+        // code here
+        for(int i = 0; i<arr.size(); i++){
+        if(key == i){
+            return arr[i];
+        }
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+    while (t--) {
+        vector<int> arr;
+        int key;
+        cin >> key;
+        cin.ignore();
+        string input;
+
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.findElementAtIndex(key, arr);
+
+        cout << res << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
