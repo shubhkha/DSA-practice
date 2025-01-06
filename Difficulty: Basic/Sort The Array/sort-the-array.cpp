ef651fc1093d81e87d69b61fc7598ea02a5f6749
shubhkha <157620793+shubhkha@users.noreply.github.com>
2025-01-06 17:50:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort The Array/sort-the-array.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sortArr(vector<int> &arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        ob.sortArr(nums);
+        for (auto i : nums)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
